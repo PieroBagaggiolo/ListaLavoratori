6e47454bfd392fef2dba1110055b5b717bd4a4ff
PieroBagaggiolo <56152505+PieroBagaggiolo@users.noreply.github.com>
2019-10-21 17:28:59 +0200

    commit

--- ListaLavoratori/Program.cs
+++ ListaLavoratori/Program.cs
@@ -13,20 +13,46 @@ namespace ListaLavoratori
         {
             List<Lavoratori> listL = new List<Lavoratori>();
 
-            string path = "";
+            string path = @"C:\Users\CORSO 44\Documents\GitHub\ListaLavoratori\ListaLavoratori\ListaLavoratori\Elementi";
             if(!Directory.Exists(path))
             {
                 Directory.CreateDirectory(path);
             }
-            string filename = "";
+            string filename = "test1.txt";
             string fullpath = Path.Combine(path, filename);
             if(File.Exists(fullpath))
             {
                 File.Create(fullpath);
             }
             int quantita = 0;
-            Console.WriteLine("Inserisci il numero di lavoratori che vuoi inserire: ");
-            quantita = Int32.Parse(Console.ReadLine());
+            do
+            {
+                //try
+                //{
+                    Console.WriteLine("Inserisci il numero di lavoratori che vuoi inserire: ");
+                    quantita = Int32.Parse(Console.ReadLine());
+                //}
+                //catch(Exception ex)
+                //{
+                //    Console.WriteLine("Riscontrata eccezzione", ex);
+                //}
+
+                
+                
+            }
+            while (quantita == 0);
+            for(int i = 0; i<quantita; i++)
+            {
+                Lavoratori lavoratore = new Lavoratori();
+                lavoratore.InsertWorker(lavoratore);
+                listL.Add(lavoratore);
+            }
+
+            foreach(var w in listL)
+            {
+                
+            }
+            
             
         }
     }
